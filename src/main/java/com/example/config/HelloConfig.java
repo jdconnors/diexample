78f33ae75bcfd1544ee4ff215e415c2dbc192287
jdconnors <jconnors@cisco.com>
2016-05-31 15:42:51 -0700

    changes as of the end of the 'Advanced Autowire Options in Spring' lesson

--- src/main/java/com/example/config/HelloConfig.java
+++ src/main/java/com/example/config/HelloConfig.java
@@ -6,6 +6,7 @@ import com.example.services.HelloWorldServiceEnglishImpl;
 import com.example.services.HelloWorldServiceSpanishImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 /**
@@ -21,13 +22,35 @@ public class HelloConfig {
 
     @Bean
     @Profile({"default","english"})
-    public HelloWorldService HelloWorldServiceEnglish(HelloWorldFactory factory) {
+    @Primary
+    public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory) {
         return factory.createHelloWorldService("en");
     }
 
     @Bean
     @Profile("spanish")
-    public HelloWorldService HelloWorldServiceSpanish(HelloWorldFactory factory) {
+    @Primary
+    public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory) {
         return factory.createHelloWorldService("es");
+    }
+
+    @Bean(name = "french")
+    public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory) {
+        return factory.createHelloWorldService("fr");
+    }
+
+    @Bean
+    public HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory) {
+        return factory.createHelloWorldService("de");
+    }
+
+    @Bean
+    public HelloWorldService helloWorldServicePolish(HelloWorldFactory factory) {
+        return factory.createHelloWorldService("pl");
+    }
+
+    @Bean
+    public HelloWorldService helloWorldServiceRussian(HelloWorldFactory factory) {
+        return factory.createHelloWorldService("ru");
     }
 }
--- src/main/java/com/example/controllers/GreetingController.java
+++ src/main/java/com/example/controllers/GreetingController.java
@@ -1,7 +1,9 @@
 package com.example.controllers;
 
 import com.example.services.HelloWorldService;
+import com.example.services.HelloWorldServiceGermanImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -12,14 +14,32 @@ public class GreetingController {
 
     private HelloWorldService helloWorldService;
 
+    private HelloWorldService helloWorldServiceGerman;
+
+    private HelloWorldService helloWorldServiceFrench;
+
     @Autowired
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
+    @Autowired
+    @Qualifier("helloWorldServiceGerman")
+    public void setHelloWorldServiceGerman(HelloWorldService helloWorldServiceGerman) {
+        this.helloWorldServiceGerman = helloWorldServiceGerman;
+    }
+
+    @Autowired
+    @Qualifier("french")
+    public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench) {
+        this.helloWorldServiceFrench = helloWorldServiceFrench;
+    }
+
     public String sayHello() {
         String greeting = helloWorldService.getGreeting();
         System.out.println(greeting);
+        System.out.println(helloWorldServiceGerman.getGreeting());
+        System.out.println(helloWorldServiceFrench.getGreeting());
         return greeting;
     }
 }
