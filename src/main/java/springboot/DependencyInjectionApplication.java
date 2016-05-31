61e7950652bcf148cd81224597ddf26119392a89
jdconnors <jconnors@cisco.com>
2016-05-31 16:43:28 -0700

    changes as of the end of the 'Spring XML Configuration With Spring Boot' lesson

--- src/main/java/springboot/DependencyInjectionApplication.java
+++ src/main/java/springboot/DependencyInjectionApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("com.example")
+//@ComponentScan("com.example")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
